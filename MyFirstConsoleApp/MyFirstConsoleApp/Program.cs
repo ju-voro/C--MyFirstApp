d989840e930eb886350d740bf095b94457fa82d0
Julia Voronetskaja <julia.voronetskaja@gmail.com>
2019-08-25 12:37:57 +0300

    Data Types

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -10,13 +10,39 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Starter
+            /*Console.WriteLine("Hello World!");
             Console.ReadLine();
             Console.WriteLine("What is your name?");
             string name;
             name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello, " + name);
+            Console.ReadLine();*/
+
+            //Data Types
+            //asking .net runtime to allocate space for particular variables of a particular type\
+
+            //declaring variables
+            int x;
+            int y;
+
+            x = 7;
+            y = 3;
+
+            Console.WriteLine(x + y);
             Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Write your first name: ");
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine(); //initializations
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+
+            //naming the variables #humanreadable #camelCase
         }
     }
 }
